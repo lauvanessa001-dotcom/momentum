7d2bcb05841387e246c9e4dbc09cd821c39c30a5
lauvanessa001-dotcom <lau.vanessa001@gmail.com>
2026-06-25 13:48:27 -0700

    Fix: polyfill Array.prototype.toReversed for Node 18 (EAS Build default)

--- metro.config.js
+++ metro.config.js
@@ -1,3 +1,10 @@
+// Polyfill Array.prototype.toReversed for Node < 20 (EAS Build default is Node 18)
+if (!Array.prototype.toReversed) {
+  Array.prototype.toReversed = function () {
+    return this.slice().reverse();
+  };
+}
+
 const { getDefaultConfig } = require("expo/metro-config");
 
 module.exports = getDefaultConfig(__dirname);
